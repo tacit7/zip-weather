7afae23a884b1d591f13112bec3753b80aee3ba4
uriel <uriel781@gmail.com>
2024-09-23 11:23:51 -0500

    chore: fix rubocop single quote

--- app/services/open_weather_map_service.rb
+++ app/services/open_weather_map_service.rb
@@ -1,11 +1,11 @@
 class OpenWeatherMapService
   include HTTParty
-  base_uri 'https://api.openweathermap.org/data/2.5'
+  base_uri "https://api.openweathermap.org/data/2.5"
 
-  API_KEY = ENV['OPENWEATHERMAP_API_KEY']
+  API_KEY = ENV["OPENWEATHERMAP_API_KEY"]
 
   def fetch_weather_by_zip(zip_code)
-    response = self.class.get("/weather", query: { zip: zip_code, appid: API_KEY, units: 'imperial' })
+    response = self.class.get("/weather", query: { zip: zip_code, appid: API_KEY, units: "imperial" })
     response.parsed_response if response.success?
   end
 end
